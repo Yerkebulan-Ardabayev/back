3d74ba2dd8701046c667fb29e221202972983620
Yerkebulan Ardabayev <yerkebulan.ardabayev@gmail.com>
2023-09-24 12:42:20 +0600

    настраивали контроллеры, сервисы

---
+++ src/config/mongo.config.ts
@@ -0,0 +1,8 @@
+import { ConfigService } from '@nestjs/config/dist';
+import { TypegooseModuleOptions } from 'nestjs-typegoose';
+
+export const getMongoDbConfig = async (
+  configService: ConfigService,
+): Promise<TypegooseModuleOptions> => ({
+  uri: configService.get('MONGO_URI'),
+});
